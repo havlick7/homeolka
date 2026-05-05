9bc26036c5abd781e3cf35b698d2ca33f2162b8a
havlick7 <vit.havlicek@gmail.com>
2026-05-05 15:36:20 +0200

    Fix production CSS build and favicon paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./index.html', './assets/js/**/*.js'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: '#D83B7D',
+        brandSoft: 'rgba(216, 59, 125, 0.12)',
+        wine: '#4A0E1C',
+        shell: '#FDFBF7'
+      }
+    }
   },
   plugins: []
 };
